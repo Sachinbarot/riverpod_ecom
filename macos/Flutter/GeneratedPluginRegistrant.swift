19b299ff36d67136e8381fe6b466b325b369ebb0
sachinbrahmbhatt <barotsachin739@gmail.com>
2024-09-27 17:19:46 +0530

    "Updated dependencies in pubspec.lock and generated_plugin_registrant.cc"

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import open_document
+import open_file_mac
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
@@ -13,6 +14,7 @@ import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   OpenDocumentPlugin.register(with: registry.registrar(forPlugin: "OpenDocumentPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
